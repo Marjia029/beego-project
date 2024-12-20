e714b25b9a166c01288b9cf6381e445c04f6662d
Marjia Afroj <marjiaafroj2@gmail.com>
2024-12-20 11:11:24 +0600

    "used channel in breed_searching"

--- controllers/breed_search.go
+++ controllers/breed_search.go
@@ -62,13 +62,12 @@ func (c *BreedSearchController) Get() {
 
 	// Set default selected breed (first breed)
 	if len(breeds) > 0 {
-		c.fetchBreedDetails(breeds[0].ID)
+		c.fetchBreedDetailsConcurrently(breeds[0].ID)
 	}
 
 	c.Data["ActiveTab"] = "breeds"
 	c.Data["Breeds"] = breeds
 	c.TplName = "breed_search.tpl"
-
 }
 
 func (c *BreedSearchController) Post() {
@@ -78,17 +77,85 @@ func (c *BreedSearchController) Post() {
 		return
 	}
 
-	c.fetchBreedDetails(breedID)
+	c.fetchBreedDetailsConcurrently(breedID)
 	c.TplName = "breed_search.tpl"
 	c.Data["ActiveTab"] = "breeds"
 }
 
-func (c *BreedSearchController) fetchBreedDetails(breedID string) {
+// Fetch breed details and images concurrently using channels
+func (c *BreedSearchController) fetchBreedDetailsConcurrently(breedID string) {
 	apiKey := "live_GWXcPdnWze27MNMJSjinKshtfsnVsi4EdrXfKUNhOmXsLakl5N7MwJCShLvC5Rxo"
 
-	// Fetch breed details
-	breedDetailsURL := "https://api.thecatapi.com/v1/breeds"
-	req, _ := http.NewRequest("GET", breedDetailsURL, nil)
+	// Channel to receive breed details
+	breedDetailsChan := make(chan CatBreed)
+	// Channel to receive breed images
+	imagesChan := make(chan []BreedImage)
+	// Channel to handle errors
+	errChan := make(chan error)
+
+	// Fetch breed details concurrently
+	go func() {
+		breedDetailsURL := "https://api.thecatapi.com/v1/breeds"
+		req, _ := http.NewRequest("GET", breedDetailsURL, nil)
+		req.Header.Set("x-api-key", apiKey)
+
+		client := &http.Client{}
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+
+		var breeds []CatBreed
+		body, _ := ioutil.ReadAll(resp.Body)
+		json.Unmarshal(body, &breeds)
+
+		var selectedBreed CatBreed
+		for _, breed := range breeds {
+			if breed.ID == breedID {
+				selectedBreed = breed
+				break
+			}
+		}
+
+		breedDetailsChan <- selectedBreed
+	}()
+
+	// Fetch breed images concurrently
+	go func() {
+		imagesURL := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?breed_ids=%s&limit=8", breedID)
+		req, _ := http.NewRequest("GET", imagesURL, nil)
+		req.Header.Set("x-api-key", apiKey)
+
+		client := &http.Client{}
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+
+		var images []BreedImage
+		body, _ := ioutil.ReadAll(resp.Body)
+		json.Unmarshal(body, &images)
+
+		imagesChan <- images
+	}()
+
+	// Wait for all goroutines to complete and handle errors
+	select {
+	case selectedBreed := <-breedDetailsChan:
+		c.Data["SelectedBreed"] = selectedBreed
+	case err := <-errChan:
+		c.Data["Error"] = err.Error()
+		c.TplName = "breed_search.tpl"
+		return
+	}
+
+	select {
+	case breedImages := <-imagesChan:
+		c.Data["BreedImages"] = breedImages
+	case err := <-errChan:
+		c.Data["Error"] = err.Error()
+		c.TplName = "breed_search.tpl"
+		return
+	}
+
+	// Fetch breeds again for the sidebar (if needed)
+	req, _ := http.NewRequest("GET", "https://api.thecatapi.com/v1/breeds", nil)
 	req.Header.Set("x-api-key", apiKey)
 
 	client := &http.Client{}
@@ -99,28 +166,5 @@ func (c *BreedSearchController) fetchBreedDetails(breedID string) {
 	body, _ := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(body, &breeds)
 
-	var selectedBreed CatBreed
-	for _, breed := range breeds {
-		if breed.ID == breedID {
-			selectedBreed = breed
-			break
-		}
-	}
-
-	// Fetch images
-	imagesURL := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?breed_ids=%s&limit=8", breedID)
-	req, _ = http.NewRequest("GET", imagesURL, nil)
-	req.Header.Set("x-api-key", apiKey)
-
-	resp, _ = client.Do(req)
-	defer resp.Body.Close()
-
-	var images []BreedImage
-	body, _ = ioutil.ReadAll(resp.Body)
-	json.Unmarshal(body, &images)
-
-	c.Data["SelectedBreed"] = selectedBreed
-	c.Data["BreedImages"] = images
 	c.Data["Breeds"] = breeds
-
 }
